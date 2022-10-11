7aed209ef5079f4e449dc26577bc753cf034f1be
kottycomputer <kottycomputer@outlook.jp>
2022-10-11 11:33:57 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
-function setup(){
-  for(let i = 0; i < 10; i++){
+function setup()
+{createCanvas(1000, 1000);
+  for(let i = 0; i < 9; i++){
     // BLANK[1]
+    let x=i*10+10
+    noFill();
+    if(i<5){stroke(0, 0, 255)}
+    else{stroke(255, 0, 0)}
+    ;
+  ellipse(100, 100, x);
+  
   }
 }
