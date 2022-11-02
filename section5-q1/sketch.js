bd2ef23fb875a9243d481cd0ff45e729a34da8ef
kottycomputer <kottycomputer@outlook.jp>
2022-11-02 13:33:12 +0900

    Change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -2,17 +2,25 @@
 // 吹き出しの位置、背景色 etc. を関数 balloon の引数で指定できるようにしてみよう
 // 吹き出しにしっぽを付けてみよう
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(500, 500);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("kottycomputer",120,50,125,0);
 }
 
-function balloon(t){
+
+
+function balloon(t,x,y,c)
+{
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  let p = 100;
+  fill(0,128,128);
+  noStroke(0);
+  ellipse(x+140, y+120, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, p+x, h + p+y);
 }
+
+function stroke(0);
+  strokeWeight( 10 );
+  line(100, 100, 150, 300);
