9d0c15ef30a3857648e1c13f437fa857428b1baf
kottycomputer <kottycomputer@outlook.jp>
2022-11-04 11:49:07 +0900

    Change section5-q1
    吹き出し

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -5,22 +5,26 @@ function setup(){
   createCanvas(500, 500);
   background(255);
   balloon("kottycomputer",120,50,125,0);
-}
+  stroke(0,128,128);
+  fill(0,128,128);
+  line(210,260,270,260)
+  line(270, 260, 170, 330);
+  line(210, 260, 170, 330);
+  triangle(210, 259, 270, 260, 170, 330);
+  }
 
 
-
-function balloon(t,x,y,c)
+function balloon(t,x,y)
 {
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 100;
   fill(0,128,128);
   noStroke(0);
-  ellipse(x+140, y+120, w + p * 2, h + p * 2);
+  ellipse(x+140, y+120, w + p * 2, h + p *1.7);
   fill(255);
   text(t, p+x, h + p+y);
 }
 
-function stroke(0);
-  strokeWeight( 10 );
-  line(100, 100, 150, 300);
+
+
