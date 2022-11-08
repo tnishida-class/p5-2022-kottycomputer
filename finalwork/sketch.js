2f1a73af3e5575f4983712eb7fd9d836b8f6136d
kottycomputer <kottycomputer@outlook.jp>
2022-11-08 12:40:26 +0900

    Change finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,11 +1,25 @@
 // 最終課題を制作しよう
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(700, 700);
+  background(138, 82, 37);
+  //円
+  fill(10);
+  //let x = 0;
+  //let y=  0;
+  for(let y=0; y< 10; y++)
+  {for(let x=0; x< 10; x++)
+    {
+  ellipse(x*100, y*100, 80);
+    }   
+   
+  }
+  //円
+
 }
 
 function draw(){
-  background(160, 192, 255);
+  
 }
 
 function windowResized(){
