a6bea0cc6a0945f829ce67d53553d56cbf061b60
kottycomputer <kottycomputer@outlook.jp>
2022-11-09 02:48:57 +0900

    Change section finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -3,18 +3,19 @@
 function setup(){
   createCanvas(700, 700);
   background(138, 82, 37);
+  
   //円
   fill(10);
   //let x = 0;
   //let y=  0;
-  for(let y=0; y< 10; y++)
-  {for(let x=0; x< 10; x++)
+  for(let y=0; y< 5; y++)
+  {for(let x=0; x< 5; x++)
     {
-  ellipse(x*100, y*100, 80);
+  ellipse(x*130+90, y*130+100, 100);
     }   
    
   }
-  //円
+  //円終わり
 
 }
 
