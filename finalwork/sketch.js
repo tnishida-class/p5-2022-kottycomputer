8b13aa3be78d88a19dc085b95015cc4099620948
kottycomputer <kottycomputer@outlook.jp>
2022-11-27 03:47:28 +0900

    Change finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,28 +1,92 @@
-// 最終課題を制作しよう
+// 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
+let speed;
+let cycle;
+let sel;
 
 function setup(){
-  createCanvas(700, 700);
-  background(138, 82, 37);
-  
-  //円
-  fill(10);
-  //let x = 0;
-  //let y=  0;
-  for(let y=0; y< 5; y++)
-  {for(let x=0; x< 5; x++)
-    {
-  ellipse(x*130+90, y*130+100, 100);
-    }   
-   
-  }
-  //円終わり
+  createCanvas(500, 500, WEBGL);
+  describe('a rotating white torus');
 
+  speed =0;
+  cycle = 80;
+  x = width / 2;
+  y = height / 2;
+  vx = 2;
+  vy = 2; 
+
+  textAlign(CENTER);
+  stroke(10)
+  sel = createSelect();
+  sel.position(200, 200);
+  sel.option('Is it a hula hoop...?');
+  sel.option('It is a donut!!!!');
+  
+  sel.changed(mySelectEventssss);
 }
+
+
+
 
 function draw(){
   
-}
+  background(247, 229, 87);
+  speed = (speed+1.5) % cycle;
+  let size = -speed;
+ 
+ 
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+  if(x>-90&&x<90&&y>-90&&y<90){
+    ellipse(x, y, size+200);
+    
+    fill(209, 6, 6)
+    ellipse(x+5,y+15,50,30)
+    fill(10)
+  ellipse(x+15,y-10,10)
+  ellipse(x,y-10,10)
+
+  fill(237, 237, 237)
+  ellipse(x+15,y-10,4)
+  ellipse(x,y-10,4)
+
+  }
+
+  else{
+    
+    ellipse(x, y, size+150);
+    fill(209, 6, 6)
+    ellipse(x+5,y,10,7)
+
+    fill(10)
+  ellipse(x+10,y-10,5)
+  ellipse(x,y-10,5)
+  }
+  
+  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
+  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
+  if(keyIsDown(UP_ARROW)){ y -= 5; }
+  if(keyIsDown(DOWN_ARROW)){ y += 5; }
+  if(keyIsDown("A".charCodeAt(0))){ 
+    speed = (speed+3) % cycle; }
+  if(keyIsDown(" ".charCodeAt(0))){ 
+    x += vx;
+    y += vy;
+  
+    // 跳ね返りは「速度 × -1」
+    if(x < 0 || x > width){ vx = -0.1 * vx; }
+    if(y < 0 || y > height){ vy = -0.1 * vy;}
+  }
+
+  fill(224, 120, 45)
+  
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(40, 20);
+  noStroke(0);
+}
+ 
+
+
+function mySelectEventssss() {
+  let item = sel.value();
+  text("it's a " + item + '!', 100, 100);
 }
